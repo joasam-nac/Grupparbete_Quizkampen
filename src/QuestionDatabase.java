d4bf7e4e424a4aae846227d3cc9ae3dbe8fa0e5d
Karwan Ali <karwana0000@gmail.com>
2025-11-24 13:47:08 +0100

    starting to read questions file

--- src/QuestionDatabase.java
+++ src/QuestionDatabase.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionDatabase {
+
+    private List<Questions> allQuestions = new ArrayList<>();
+
+    public QuestionDatabase() {
+        readQuestionsFromFile(Path.of("src/questions.txt"));
+    }
+
+    public void readQuestionsFromFile(Path path) {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+
+        }
+    }
 }
